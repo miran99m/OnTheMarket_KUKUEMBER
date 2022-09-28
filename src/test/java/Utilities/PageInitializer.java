7ac0bfd2dd87ebdbc1c82f1acdac364640e905df
gulfire <gulfire.csk@gmail.com>
2022-09-28 15:31:51 -0400

    adding my tasks

--- src/test/java/Utilities/PageInitializer.java
+++ src/test/java/Utilities/PageInitializer.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
 import pages.HomePage;
+import pages.RetirementPropertiesPageObj;
 import pages.SoldPricesPageObj;
 import stepDefinition.Miran_SoldHouse;
 
@@ -13,10 +14,13 @@ public class PageInitializer extends BaseClass {
 
 	public static CommonMethods cm;
 	public static SoldPricesPageObj sp;
-
+	public static HomePage hp;
+	public static RetirementPropertiesPageObj rp;
 	
 	public static void initialize() {
 		cm = new CommonMethods();
 		sp = new SoldPricesPageObj();
+		hp = new HomePage();
+		rp = new RetirementPropertiesPageObj();
 	}
 }
